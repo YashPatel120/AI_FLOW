8757c2dde4c33a3f8c91231c743d4c6ff6567d5b
YashPatel120 <gamdhayash@gmail.com>
2026-01-04 19:56:08 +0530

    Logs added

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
 require("./config/db")();
+console.log(process.env.MONGO_URI,"process.env.MONGO_URI");
 
 app.listen(5000,()=>console.log("Server running"));
